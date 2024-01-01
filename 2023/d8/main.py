3f68eafbcddf9d108348025de95815982a8f8c47
Oscar Melin <oscar.a.melin@outlook.com>
2024-01-01 17:37:53 +0100

    day 8 p2

--- 2023/d8/main.py
+++ 2023/d8/main.py
@@ -1,32 +1,56 @@
 import sys
+import math
+
 with open(sys.argv[1] ,"r") as f:
     D = f.read().split("\n")
 
-instructions = D[0]
+directions = D[0]
 D = D[2:]
 M = {}
+positions = []
 for item in D:
     k, pair = item.split("=")
     k=k[:-1]
+    if k.endswith("A"):
+        positions.append(k)
     pair = pair[2:-1]
     p1, p2 = pair.split(",")
     p2 = p2[1:]
     M[k] = (p1,p2)
 
-    
-key = "AAA"
-ans=0
-while True:
-    for instr in instructions:
-        i=-1
-        if instr=="R":
-            i=1
-        elif instr=="L":
-            i=0
 
-        new_key=M[key][i]
-        if new_key=="ZZZ":
-            print(ans+1)
-            exit()
-        key=new_key
-        ans+=1
+
+for part2 in [False, True]:
+    cycles = []
+    for position in positions:
+        cycle = []
+        seen = set([(position, directions[0], 0)])
+        steps = 0
+        while True:
+            for di, direction in enumerate(directions):
+                # print("from", position,end="")
+                position = M[position][1 if direction=="R" else 0]
+                # print("-->",position)
+                steps += 1
+
+                if position.endswith("Z" if part2 else "ZZZ"):
+                    cycle.append(steps)
+                    if not part2:
+                        break
+
+                if (position, direction, di) in seen:
+                    break
+                seen.add((position, direction, di))
+            else:
+                continue
+            break
+
+        cycles.append(cycle)
+
+    ans = 0
+    if part2:
+        ans = math.lcm(*[x[0] for x in cycles])
+    else:
+        ans = [x[0] for x in cycles if x][0]
+    print("p1" if not part2 else "p2", ans)
+
