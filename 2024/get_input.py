6b70c1a4ddfc12684dbfb04b0fcc08ae12230535
oscarm3l1n <oscar.a.melin@outlook.com>
2024-11-26 20:34:47 +0100

    get input

--- 2024/get_input.py
+++ 2024/get_input.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
 import argparse
 import subprocess
-import sys
-import requests
+import pathlib
 
-SESSION = '<FILL_ME_IN>'
+input_file = "input.in"
 
-useragent = 'https://github.com/oscarm3l1n/advent-of-code/blob/master/get_input.py by oscar.a.melin@outlook.com'
-parser = argparse.ArgumentParser(description='Read input')
-parser.add_argument('--year', type=int, default=2024)
-parser.add_argument('--day', type=int, default=1)
+
+def prepare(day) -> pathlib.Path:
+    cwd = pathlib.Path().cwd()
+    dir = cwd / f"day{day}"
+    _input = dir / input_file
+    print(f"Creating dir {dir=}")
+    print(f"Creating file {_input=}")
+    dir.mkdir()
+    _input.write_text("")
+    return _input
+
+
+SESSION = "<FILL ME IN>"
+
+useragent = (
+    "https://github.com/oscarm3l1n/advent-of-code/blob/main/2024"
+    "/get_input.py by oscarmelin1995@gmail.com"
+)
+parser = argparse.ArgumentParser(description="Read input")
+parser.add_argument("-y", "--year", type=int, default=2024)
+parser.add_argument("-d", "--day", type=int, default=1)
 args = parser.parse_args()
 
-cmd = f'curl https://adventofcode.com/{args.year}/day/{args.day}/input --cookie "session={SESSION}" -A \'{useragent}\''
+input_file = prepare(args.day)
+
+cmd = (
+    f"curl https://adventofcode.com/{args.year}/day/{args.day}/input"
+    f'--cookie "session={SESSION}" -A "{useragent}"'
+)
 output = subprocess.check_output(cmd, shell=True)
-output = output.decode('utf-8')
-print(output, end='')
-print('\n'.join(output.split('\n')[:10]), file=sys.stderr)
+output = output.decode("utf-8")
+input_file.write_text(output)
+print(output, end="")
