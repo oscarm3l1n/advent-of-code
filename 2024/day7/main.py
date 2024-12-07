0194390aad89990edbbb5e2ba20b1a01b1b45ee1
Oscar Melin <Oscar.Melin@partner.bmw.de>
2024-12-07 07:59:06 +0100

    day 7 pt 2

--- 2024/day7/main.py
+++ 2024/day7/main.py
@@ -8,89 +8,27 @@ with open(sys.argv[1]) as f:
 
 lines = D.split('\n')
 
+def _f(target, lst):
+    if len(lst)==1:
+        return next(iter(lst)) == target
+    if _f(target, [int(str(lst[0])+str(lst[1]))] + lst[2:]):
+        return True
+    if _f(target, [lst[0]*lst[1]] + lst[2:]):
+        return True
+    if _f(target, [lst[0]+lst[1]] + lst[2:]):
+        return True
 
-ops = ['+', '*', '|']
 
-
-def lst2str(lst):
-    return ''.join(lst)
-
-
-# 292: 11 6 16 20
-def _eval(s):
-    ops_cnt = 0
-    i = 0
-    tot = 0
-    while True:
-        if ops_cnt == 1 and not i < len(s):
-            if '|' in s[:i]:
-                x = s[:i].replace('|', '')
-            else:
-                x = eval(s[:i])
-            tot += int(x)
-            break
-        if not i < len(s):
-            break
-        if s[i] in ops:
-            ops_cnt += 1
-        if ops_cnt == 2:
-            ops_cnt = 0
-            if '|' in s[:i]:
-                x = s[:i].replace('|', '')
-            else:
-                x = eval(s[:i])
-            new_s = f'{x}{s[i:]}'
-            i = 0
-            s = new_s
-        i += 1
-    return tot
 
 
 correct = []
-lne = 0
-for line in lines:
-    print(f'line {lne} of {len(lines)}')
-    lne += 1
+ans = 0
+for i,line in enumerate(lines):
+    print(f"{i=} of {len(lines)}")
     Y, xs = line.split(':')
     Y = int(Y)
     xs = [int(x) for x in xs.split()]
-    s = ''
-    for i, x in enumerate(xs):
-        if i == len(xs) - 1:
-            s += f'{x}'
-            continue
-        s += f'{x}+'
-    s = [ch for ch in s]
-    Q = [s]
-    seen = set()
-    while Q:
-        curr = Q.pop()
-        seen.add(lst2str(curr))
-        if Y == _eval(lst2str(curr)):
-            correct.append((_eval(lst2str(curr)), lst2str(curr), line))
-        else:
-            for i, ch in enumerate(curr):
-                if ch in ops:
-                    op = '+'
-                    test = curr[:i] + [op] + curr[i + 1 :]
-                    if lst2str(test) not in seen:
-                        Q.append(test)
-                    op = '*'
-                    test = curr[:i] + [op] + curr[i + 1 :]
-                    if lst2str(test) not in seen:
-                        Q.append(test)
+    if _f(Y, xs):
+        ans += Y
 
-                    op = '|'
-                    test = curr[:i] + [op] + curr[i + 1 :]
-                    if lst2str(test) not in seen:
-                        Q.append(test)
-
-
-ans = 0
-seen = set()
-for i in range(len(correct)):
-    a, b, c = correct[i]
-    if c not in seen:
-        ans += a
-        seen.add(c)
 print(ans)
