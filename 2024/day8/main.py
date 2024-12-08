c698497140979700ad4b4a438b08e8271391c32c
Oscar Melin <Oscar.Melin@partner.bmw.de>
2024-12-08 13:49:15 +0100

    day 8 p1

---
+++ 2024/day8/main.py
@@ -0,0 +1,43 @@
+import sys
+from collections import defaultdict, Counter
+
+with open(sys.argv[1]) as f:
+    D = f.read().strip()
+
+
+lines = D.split('\n')
+
+G = [line for line in lines]
+R = len(G)
+C = len(G[0])
+M = defaultdict(set[tuple])
+
+for r in range(R):
+    for c in range(C):
+        ch = G[r][c]
+        if ch != '.':
+            M[ch].add((r,c))
+
+nodes = set()
+for ch in M:
+    pts = [p for p in M[ch]]
+    for i in range(len(pts)):
+        for j in range(len(pts)):
+            if i == j:
+                continue
+            p1 = pts[i]
+            p2 = pts[j]
+            dr = p1[0]-p2[0]
+            dc = p1[1]-p2[1]
+            # need to calculate manhattan dist
+            rr = p1[0]+dr
+            cc = p1[1]+dc
+            if 0<=rr<R and 0<=cc<C:# and G[rr][cc] == '.':
+                nodes.add((rr,cc))
+print("p1", len(nodes))
+
+
+
+
+
+
