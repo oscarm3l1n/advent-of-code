07aa565502fec9b433b10980cf86ce36d26c1910
Oscar Melin <Oscar.Melin@partner.bmw.de>
2024-12-09 21:58:07 +0100

    day 2 complete

--- 2024/day9/main.py
+++ 2024/day9/main.py
@@ -16,105 +16,80 @@ for ch in D:
     if free_space:
         for i in range(ch):
             s.append('.')
-        ss.append(('.', free_space, ch))
+        ss.append(('.', ch))
     else:
         for i in range(ch):
             s.append(str(idx))
-        stack.append(str(idx)*ch)
-        ss.append((f'{idx}', free_space, ch))
+        ss.append((str(idx), ch))
         idx+=1
     free_space = not free_space
 
-#le = 0
-#ri = len(s)-1
-#while True:
-#    print(f'{le} of {len(s)}')
-#    for i in range(len(s)):
-#        if s[i]=='.':
-#            le = i
-#            break
-#    for i in reversed(range(len(s))):
-#        if s[i].isdigit():
-#            ri = i
-#            break
-#
-#    if le > ri:
-#        break
-#    s[le] = s[ri]
-#    s[ri] = '.'
-#
-#ans = 0
-#for i in range(len(s)):
-#    if s[i]=='.':
-#        break
-#    ans += i*int(s[i])
-#print(ans)
-
-
 def pr(s):
-    print(''.join(s))
+    for ch,size in s:
+        print(ch*size,end='')
+    print()
+pr(ss)
 
-pr(s)
+class Block:
+    def __init__(self, tup):
+        self.tup = tup
+    @property
+    def size(self):
+        return self.tup[1]
+    @property
+    def char(self):
+        return self.tup[0]
+    def __repr__(self) -> str:
+        return f'Block(char={self.char}, size={self.size})'
 
-C = defaultdict(int)
-pts = defaultdict(tuple)
 
-for i in reversed(range(len(s))):
-    C[s[i]] += 1
-
-i = 0
-while i < len(s):
-    ch = s[i]
-    if ch == '.':
-        # find slutpunkt
-        for le in range(i, len(s)):
-            if s[le] != '.':
-                pts[i] = (i, le-1)
-                i = le
+i = len(ss)-1
+while i >= 0:
+    print(i)
+    back = Block(ss[i])
+    if back.char != '.':
+        for j in range(len(ss)):
+            front = Block(ss[j])
+            if i < j:
                 break
-    i += 1
+            if back.size < front.size and front.char == '.':
+                #print('reverse is smaller than front')
+                # remove old pos
+                ss.pop(j)
+                # insert new block
+                ss.insert(j, (back.char, back.size))
+                # insert empty block
+                ss.insert(j+1, ('.', front.size - back.size))
+                # remove old block
+                ss.pop(i+1)
+                # insert dots at old pos
+                ss.insert(i+1, ('.', back.size))
+                break
+            elif back.size == front.size and front.char == '.':
+                #print('back == front')
+                # remove old block
+                ss.pop(j)
+                # insert new block
+                ss.insert(j, (back.char, back.size))
+                # remove old block
+                ss.pop(i)
+                # insert dots at old pos
+                ss.insert(i, ('.', back.size))
+                break
+    i -= 1
 
-class Stop(Exception):
-    """time to stop"""
+lst = []
+for x in ss:
+    b = Block(x)
+    for _ in range(b.size):
+        lst.append(b.char)
 
-prev = s[-1]
-try:
-    for i in reversed(range(len(s))):
-        ch = s[i]
-        if ch != prev and prev != '.':
-            curr = C[prev]
-            for k in pts:
-                start, end = pts[k]
-                size = (end-start)+1
-                if curr <= size:
-                    if start+curr > end:
-                        pass
-                    #print(f'{i=}')
-                    #print("".join(s))
-                    #print(f'{" "*start}^')
-                    #print(f'{prev=} {C[prev]}')
-                    #print(f'{s[start:end]=}')
-                    #print(f'{start=} {end=}')
-                    #print(f'{size=}')
-                    #print(f'{C[prev]=}')
-                    if start > i:
-                        raise Stop
-                    for c in range(curr):
-                        s[start+c] = prev
-                    pts[k] = (start+curr, end)
-                    # clean up
-                    for z in range(i+1, i+curr+1):
-                        s[z] = '.'
-                    break
-        prev = ch
-except Stop:
-    for k in pts:
-        print(pts[k])
-    ans = 0
-    for i in range(len(s)):
-        ch = s[i]
-        if ch != '.':
-            ans += i*int(ch)
-    print(6412390114238 - ans)
-    print(ans)
+ans = 0
+for i in range(len(lst)):
+    if lst[i] != '.':
+        ans += i*int(lst[i])
+print("p2", ans)
+
+
+
 
