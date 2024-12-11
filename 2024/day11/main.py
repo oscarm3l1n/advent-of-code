d4a72dcca560c092da1531720d3db1dc2c932a5f
Oscar Melin <Oscar.Melin@partner.bmw.de>
2024-12-11 08:24:07 +0100

    day 11 p2

--- 2024/day11/main.py
+++ 2024/day11/main.py
@@ -6,55 +6,57 @@ with open(sys.argv[1]) as f:
 
 xs = [int(x) for x in D.split()]
 
-# number on a stone can change
-
-# a stone can split in two
-
-# these rules are in order
-
 """
 1. if stone == 0 --> replace with 1
 2. if stone == len(str(stone)) % 2 == 0 --> 100000 == 10 0 (leading zeroes are stripped)
 3. multiply by 2024
 """
 
-def r2(s):
-    return (len(str(s)) % 2) == 0
-def r1(s):
-    return s == 0
+# if x == 0:
+#     xs[i] = 1
+# elif len(str(x)) % 2 == 0
+#     s = xs.pop(i)
+#     s = str(s)
+#     n = len(s)//2
+#     le = s[n:]
+#     ri = s[:n]
+#     xs.insert(i, int(le))
+#     xs.insert(i+1,int(ri))
+#     i+=1
+# else:
+#     xs[i] *= 2024
 
-N = 25
-for _ in range(N):
-    print(f'{_} / {N}')
-    #print(xs)
-    i = 0
-    while i < len(xs):
-        st = xs[i]
 
-        if r1(st):
-            xs[i] = 1
-        elif r2(st):
-            s = xs.pop(i)
-            s = str(s)
-            n = len(s)//2
-            if n*2 == 2:
-                le = s[0]
-                ri = s[1]
-            else:
-                le = s[:n]
-                ri = s[n:]
-            #print(f'{le=} {ri=} {st=}')
-            #print(xs)
-            xs.insert(i, int(le))
-            xs.insert(i+1,int(ri))
-            #print(xs)
-            #print("-----")
-            i+=1
-        else:
-            xs[i] *= 2024
-        i+=1
+M = {}
 
-print("p1", len(xs))
 
-# 100 --> 10 0
-# 100/10
+# would also work with
+# from functools import cache
+# @cache
+def calc(x, i, N):
+    if (x, i, N) in M:
+        return M[(x, i, N)]
+    if i == N:
+        ans = 1
+    elif x == 0:
+        ans = calc(1, i + 1, N)
+    elif len(str(x)) % 2 == 0:
+        s = str(x)
+        slen = len(s)
+        le = s[: slen // 2]
+        ri = s[slen // 2 :]
+        le = int(le)
+        ri = int(ri)
+        ans = calc(le, i + 1, N) + calc(ri, i + 1, N)
+    else:
+        ans = calc(2024 * x, i + 1, N)
+    M[(x, i, N)] = ans
+    return ans
+
+
+for N in (25, 75):
+    ans = 0
+    for i in range(len(xs)):
+        print(f'{i}/{len(xs)}')
+        ans += calc(xs[i], 0, N)
+    print('p1' if N == 25 else 'p2', ans)
