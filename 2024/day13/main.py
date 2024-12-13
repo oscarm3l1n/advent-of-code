2367f70880a8bee5fab320c3901b025ee039e60a
Oscar Melin <Oscar.Melin@partner.bmw.de>
2024-12-13 23:37:13 +0100

    day 13

--- 2024/day13/main.py
+++ 2024/day13/main.py
@@ -65,40 +65,53 @@ def solve(b1: Button, b2: Button, prize_pos: Prize):
             # not possible
             return float('inf')
         if x == tx and y == ty:
-            print(f'{da=} {db=}')
-            print(f'{cost_a=} {cost_b=}')
-            print(f'{da*cost_a=} {db*cost_b=}')
             return da * cost_a + db * cost_b
         ans = float('inf')
         if x + ax <= tx and y + ay <= ty:
             ans = min(ans, dfs(x + ax, y + ay, da + 1, db))
         if x + bx <= tx and y + by <= ty:
             ans = min(ans, dfs(x + bx, y + by, da, db + 1))
-        # print(f'{x+ax <= tx and y+ay <= ty}')
-        # print(f'{x+bx <= tx and y+by <= ty}')
         M[key] = ans
         return ans
 
     return dfs(0, 0, 0, 0)
 
 
+def solve2(a, b, p):
+    """
+    Iax + Ubx = Px
+    Iay + Uby = Py
+    -->
+    Iaxby + Ubxby = Pxby
+    Iaybx + Ubxby = Pybx
+    -->
+    Iaxby - Iaybx = Pxby-Pybx
+    I(axby - aybx) = Pxby-Pybx
+    I = (Pxby - Pybx) / (axby - aybx)
+    """
+
+    ax, bx, ay, by, px, py = a.x, b.x, a.y, b.y, p.x, p.y
+    px += 10000000000000
+    py += 10000000000000
+    I = (py * bx - px * by) / (ay * bx - ax * by)
+    U = (px - ax * I) / bx
+    return I * 3 + U
+
+
 ans = 0
+p2 = 0
 for seg in segments:
-    xy_pat = re.compile(r'Button \w: X\+(?P<X>.*\d+?).*Y\+(?P<Y>.*\d+?)')
-    prize_pat = re.compile(r'Prize: X=(?P<PX>.*\d+?).*Y=(?P<PY>.*\d+?)')
-
-    print('SEG')
-    print(seg)
-
     A = Button.parse(seg, is_a=True)
     B = Button.parse(seg, is_a=False)
     PRIZE = Prize.parse(seg)
-    tx, ty = PRIZE.x, PRIZE.y
-    ax, ay = A.x, A.y
-    bx, by = B.x, B.y
-    ac, bc = A.cost, B.cost
 
     x = solve(A, B, PRIZE)
     if x != float('inf'):
         ans += x
+
+    x = solve2(A, B, PRIZE)
+    if x % 1 == 0:
+        p2 += x
+
 print('p1', ans)
+print('p2', int(p2))
