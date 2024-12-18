f07d0dd8246bc0cdf52d8fe91817a846cdd7411e
Oscar Melin <Oscar.Melin@partner.bmw.de>
2024-12-18 16:44:41 +0100

    day 16 p2

--- 2024/day16/main.py
+++ 2024/day16/main.py
@@ -5,23 +5,6 @@ from heapq import heappop, heappush
 with open(sys.argv[1], 'r') as f:
     D = f.read().strip()
 
-# D="""#################
-##...#...#...#..E#
-##.#.#.#.#.#.#.#.#
-##.#.#.#...#...#.#
-##.#.#.#.###.#.#.#
-##...#.#.#.....#.#
-##.#.#.#.#.#####.#
-##.#...#.#.#.....#
-##.#.#####.#.###.#
-##.#.#.......#...#
-##.#.###.#####.###
-##.#.#...#.....#.#
-##.#.#.#####.###.#
-##.#.#.........#.#
-##.#.#.#########.#
-##S#.............#
-##################"""
 
 G = [line for line in D.split('\n')]
 R, C = len(G), len(G[0])
@@ -42,40 +25,41 @@ def ok(r, c):
     return 0 <= r < R and 0 <= c < C and G[r][c] != '#'
 
 
-pq = []
-dirs = [west, north, east, south]
-d = 2
-heappush(pq, (0, sr, sc, d, set()))
-seen = set()
+def part_1(fill_me):
+    pq = []
+    dirs = [west, north, east, south]
+    d = 2
+    heappush(pq, (0, sr, sc, d, set()))
+    seen = {}
+    best = float('inf')
+    while pq:
+        cost, r, c, d, m = heappop(pq)
+        if G[r][c] == 'E' and cost <= best:
+            best = min(best, cost)
+            fill_me |= m
+        if (r, c, d) in seen and cost > seen[r, c, d]:
+            continue
+        seen[r, c, d] = cost
+        nm = m.copy()
+        nm.add((r, c))
+        dr, dc = dirs[d]
+        rr, cc = r + dr, c + dc
+        if ok(rr, cc):
+            heappush(pq, (cost + 1, rr, cc, d, nm))
+        heappush(pq, (cost + 1000, r, c, (d - 1) % 4, nm))
+        heappush(pq, (cost + 1000, r, c, (d + 1) % 4, nm))
+    return best
+
+
+def part_2(best_paths):
+    ans = 0
+    for r in range(R):
+        for c in range(C):
+            if (r, c) in best_paths:
+                ans += 1
+    return ans + 1
+
+
 best_paths = set()
-while pq:
-    cost, r, c, d, m = heappop(pq)
-    if G[r][c] == 'E':
-        print('GOAL', cost)
-        best_paths |= m
-    if (r, c, d) in seen:
-        continue
-    seen.add((r, c, d))
-    nm = m.copy()
-    nm.add((r, c))
-    dr, dc = dirs[d]
-    rr, cc = r + dr, c + dc
-    if ok(rr, cc):
-        heappush(pq, (cost + 1, rr, cc, d, nm))
-    heappush(pq, (cost + 1000, r, c, (d - 1) % 4, nm))
-    heappush(pq, (cost + 1000, r, c, (d + 1) % 4, nm))
-
-for r in range(R):
-    for c in range(C):
-        if (r, c) in best_paths:
-            print('O', end='')
-        else:
-            print(G[r][c], end='')
-    print()
-
-p2 = 0
-for r in range(R):
-    for c in range(C):
-        if (r, c) in best_paths:
-            p2 += 1
-print(p2)
+print(part_1(best_paths))
+print(part_2(best_paths))
