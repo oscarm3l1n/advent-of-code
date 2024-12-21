370e58987e00c23fefeee759e8b7f0a658561465
Oscar Melin <Oscar.Melin@partner.bmw.de>
2024-12-21 17:46:29 +0100

    format

--- 2024/day20/main.py
+++ 2024/day20/main.py
@@ -1,77 +1,80 @@
 import sys
 from collections import deque, Counter
 
-dirs4 = [(1,0),(-1,0),(0,1),(0,-1)]
+dirs4 = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
-with open(sys.argv[1], "r") as f:
+with open(sys.argv[1], 'r') as f:
     D = f.read().strip()
 
-G = [line for line in D.split("\n")]
+G = [line for line in D.split('\n')]
 R = len(G)
 C = len(G[0])
 
-sr,sc = None, None
-tr,tc = None, None
+sr, sc = None, None
+tr, tc = None, None
 
 for r in range(R):
     for c in range(C):
-        if G[r][c] == "S":
-            sr,sc = r,c
-        if G[r][c] == "E":
-            tr,tc = r,c
+        if G[r][c] == 'S':
+            sr, sc = r, c
+        if G[r][c] == 'E':
+            tr, tc = r, c
 
 assert sr is not None
 assert sc is not None
 assert tr is not None
 assert tr is not None
 
-def ok(r,c, ok_pos):
-    if (r,c) == ok_pos:
+
+def ok(r, c, ok_pos):
+    if (r, c) == ok_pos:
         return True
-    return 0<=r<R and 0<=c<C and G[r][c] != "#"
+    return 0 <= r < R and 0 <= c < C and G[r][c] != '#'
 
 
 def find_shortest():
-    Q = deque([(sr,sc,0)])
+    Q = deque([(sr, sc, 0)])
     seen = set()
     while Q:
-        r,c,cost = Q.popleft()
-        if (r,c) == (tr,tc):
+        r, c, cost = Q.popleft()
+        if (r, c) == (tr, tc):
             return cost
-        if (r,c) in seen:
+        if (r, c) in seen:
             continue
-        seen.add((r,c))
-        for dr,dc in dirs4:
-            rr,cc = r+dr, c+dc
-            if ok(rr,cc, (-1,-1)):
-                Q.append((rr,cc, cost+1))
+        seen.add((r, c))
+        for dr, dc in dirs4:
+            rr, cc = r + dr, c + dc
+            if ok(rr, cc, (-1, -1)):
+                Q.append((rr, cc, cost + 1))
     return saved
 
-def bfs(sr,sc, curr, n):
-    Q = deque([(sr,sc,0)])
+
+def bfs(sr, sc, curr, n):
+    Q = deque([(sr, sc, 0)])
     seen = set()
     saved = Counter()
     while Q:
-        r,c,cost = Q.popleft()
-        if (r,c) == (tr,tc):
-            saved[n-cost] += 1
+        r, c, cost = Q.popleft()
+        if (r, c) == (tr, tc):
+            saved[n - cost] += 1
             break
-        if (r,c) in seen:
+        if (r, c) in seen:
             continue
-        seen.add((r,c))
-        for dr,dc in dirs4:
-            rr,cc = r+dr, c+dc
-            if ok(rr,cc, curr):
-                Q.append((rr,cc, cost+1))
+        seen.add((r, c))
+        for dr, dc in dirs4:
+            rr, cc = r + dr, c + dc
+            if ok(rr, cc, curr):
+                Q.append((rr, cc, cost + 1))
     return saved
+
 
 n = find_shortest()
 saved = Counter()
 for r_ in range(R):
     print(f'{r_}/{R}')
     for c_ in range(C):
-        nsaved = bfs(sr,sc,(r_,c_),n)
+        nsaved = bfs(sr, sc, (r_, c_), n)
         for k in nsaved:
             saved[k] += nsaved[k]
 
--- 2024/day20/p2.py
+++ 2024/day20/p2.py
@@ -2,87 +2,91 @@ import sys
 from collections import deque, Counter
 import heapq
 
-dirs4 = [(1,0),(-1,0),(0,1),(0,-1)]
+dirs4 = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-is_test = sys.argv[1] == "test.in"
-with open(sys.argv[1], "r") as f:
+is_test = sys.argv[1] == 'test.in'
+with open(sys.argv[1], 'r') as f:
     D = f.read().strip()
 
-G = [line for line in D.split("\n")]
+G = [line for line in D.split('\n')]
 R = len(G)
 C = len(G[0])
 
-sr,sc = -1,-1
-tr,tc = -1,-1
+sr, sc = -1, -1
+tr, tc = -1, -1
 
 for r in range(R):
     for c in range(C):
-        if G[r][c] == "S":
-            sr,sc = r,c
-        if G[r][c] == "E":
-            tr,tc = r,c
+        if G[r][c] == 'S':
+            sr, sc = r, c
+        if G[r][c] == 'E':
+            tr, tc = r, c
 
 MAX_CHEATS = 20
 
 
 def manhattan(p1, p2):
-    x1,y1 = p1
-    x2,y2 = p2
-    return abs(x1-x2) + abs(y1-y2)
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
 
 def find_shortest():
-    Q = deque([(sr,sc,0)])
+    Q = deque([(sr, sc, 0)])
     seen = set()
-    def ok(r,c):
-        return 0<=r<R and 0<=c<C and G[r][c] != "#"
+
+    def ok(r, c):
+        return 0 <= r < R and 0 <= c < C and G[r][c] != '#'
+
     while Q:
-        r,c,cost = Q.popleft()
-        if (r,c) == (tr,tc):
+        r, c, cost = Q.popleft()
+        if (r, c) == (tr, tc):
             return cost
-        if (r,c) in seen:
+        if (r, c) in seen:
             continue
-        seen.add((r,c))
-        for dr,dc in dirs4:
+        seen.add((r, c))
+        for dr, dc in dirs4:
             assert r is not None and c is not None
-            rr,cc = r+dr, c+dc
-            if ok(rr,cc):
-                Q.append((rr,cc, cost+1))
+            rr, cc = r + dr, c + dc
+            if ok(rr, cc):
+                Q.append((rr, cc, cost + 1))
     return -1
 
+
 def calc_dists(from_):
-    sr,sc = from_
-    Q = deque([(sr,sc,0)])
+    sr, sc = from_
+    Q = deque([(sr, sc, 0)])
     seen = {}
     while Q:
-        r,c,cost = Q.popleft()
-        if (r,c) in seen:
+        r, c, cost = Q.popleft()
+        if (r, c) in seen:
             continue
-        seen[r,c] = cost
-        for dr,dc in dirs4:
-            rr, cc = r+dr, c+dc
-            if 0<=rr<R and 0<=cc<C and G[rr][cc] != "#":
-                Q.append((rr,cc,cost+1))
+        seen[r, c] = cost
+        for dr, dc in dirs4:
+            rr, cc = r + dr, c + dc
+            if 0 <= rr < R and 0 <= cc < C and G[rr][cc] != '#':
+                Q.append((rr, cc, cost + 1))
     return seen
 
-dist2start = calc_dists((sr,sc))
-dist2end = calc_dists((tr,tc))
+
+dist2start = calc_dists((sr, sc))
+dist2end = calc_dists((tr, tc))
 shortest = find_shortest()
 SAVE = 50 if is_test else 100
 
 
 saved = Counter()
 i = 0
-for r,c in dist2start:
+for r, c in dist2start:
     print(f'{i}/{len(dist2start)}')
-    for rr,cc in dist2end:
-        if (x:=manhattan((r,c),(rr,cc))) <= MAX_CHEATS:
-            d = dist2start[r,c] + dist2end[rr,cc] + x
+    for rr, cc in dist2end:
+        if (x := manhattan((r, c), (rr, cc))) <= MAX_CHEATS:
+            d = dist2start[r, c] + dist2end[rr, cc] + x
             if d <= shortest - SAVE:
-                saved[shortest-d] += 1
+                saved[shortest - d] += 1
     i += 1
 ans = 0
 for k in saved:
     if k >= SAVE:
         ans += saved[k]
 print(ans)
-
