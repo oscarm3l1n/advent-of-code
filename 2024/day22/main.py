2ac8494ca746dbb36f29d1985377396f59275eb7
oscarm3l1n <oscar.a.melin@outlook.com>
2024-12-22 21:37:21 +0100

    day 22 p2

--- 2024/day22/main.py
+++ 2024/day22/main.py
@@ -1,11 +1,9 @@
 import sys
-from collections import deque, namedtuple
+from collections import defaultdict, deque, Counter
 
 with open(sys.argv[1], 'r') as f:
     D = f.read().strip()
 lines = D.split('\n')
-
-Item = namedtuple('Item', ['diff', 'val'])
 
 
 def prune(x):
@@ -29,68 +27,31 @@ def run(x):
 
 
 ans = 0
-Q = deque([])
-all_seqs = []
-sequences = {}
-for i, line in enumerate(lines):
+C = Counter()
+for line in lines:
     x = int(line)
-    seqs = {}
+    Q = deque([])
+    sequences = {}
     for i in range(2000):
+        prev = x % 10
         nx = run(x)
-        xstr = str(nx)
-        if len(Q) == 4:
-            item = Item(diff=int(xstr[-1]) - Q[-1].val, val=int(xstr[-1]))
-            Q.popleft()
-            Q.append(item)
-            key = (Q[0].diff, Q[1].diff, Q[2].diff, Q[3].diff)
-            if key not in seqs:
-                seqs[key] = Q[-1].val
-        else:
-
-            if i == 0:
-                item = Item(diff=int(xstr[-1]) - int(lines[0][-1]), val=int(xstr[-1]))
-            else:
-                item = Item(diff=int(xstr[-1]) - Q[-1].val, val=int(xstr[-1]))
-            Q.append(item)
+        curr = nx % 10
+        if len(Q) < 4:
+            Q.append(curr - prev)
             if len(Q) == 4:
-                key = (Q[0].diff, Q[1].diff, Q[2].diff, Q[3].diff)
-                if key not in seqs:
-                    seqs[key] = Q[-1].val
+                key = (Q[0], Q[1], Q[2], Q[3])
+                if key not in sequences:
+                    sequences[key] = curr
+        else:
+            Q.popleft()
+            Q.append(curr - prev)
+            key = (Q[0], Q[1], Q[2], Q[3])
+            if key not in sequences:
+                sequences[key] = curr
         x = nx
-    all_seqs.append(seqs)
+    for k, v in sequences.items():
+        C[k] += v
+
     ans += x
 print('p1', ans)
-
-# now we have built sequeneces
-best = -(10**6)
-seen = set()
-it = 0
-best_seq = None
-for seqs in all_seqs:
-    print(f'{it}/{len(all_seqs)}')
-    for seq in seqs:
-        if seq in seen:
-            continue
-        seen.add(seq)
-        tot = 0
-        for i in range(len(all_seqs)):
-            tot += all_seqs[i].get(seq, 0)
-        # best = max(best, tot)
-        if tot > best:
-            best = tot
-            best_seq = seq
-    it += 1
-print(best)
-print(best_seq)
-
-it = iter(all_seqs)
-tot = 0
-try:
-    while True:
-        try:
-            tot += next(it)[best_seq]
-        except KeyError:
-            pass
-except StopIteration:
-    pass
-print(tot)
+print('p2', max(C.values()))
