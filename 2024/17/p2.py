1fad6d5cb85907c09cb5de1566b64fbacaa420e9
oscarm3l1n <oscar.a.melin@outlook.com>
2024-12-23 20:03:30 +0100

    day 17 p2

--- 2024/17/p2.py
+++ 2024/17/p2.py
@@ -1,66 +1,53 @@
-
-out = lambda x
-
-a,b,c = 0,0,0
-prog = [2,4,1,5,7,5,1,6,0,3,4,6,5,5,3,0]
-
-# 2, 4
-b = a % 8
-# 1, 5
-b = b ^ 5
-# 7, 5
-c = a >> b
-# 1, 6
-b = b ^ 6
-# 0, 3
-a = a >> 3
-# 4, 6
-b = b ^ c
-# 5, 5
-out( b % 8 )
-# 3 0
-# if a != 0 jump 0
+test = [0, 3, 5, 4, 3, 0]
 
 
-# A, B, C = 4, 5, 6
+def test_prog(a, b, c) -> list[int]:
+    out = []
+    while a != 0:
+        a = a >> 3
+        out.append(a % 8)
+    return out
 
-# 0: A = A >> combo
-# 1: B = B ^ literal
-# 2: B = combo % 8
-# 3: if A == 0 print out else jump to literal
-# 4: B = B ^ C
-# 5: out ( combo % 8 )
-# 6: B = A >> combo
-# 7: C = A >> combo
 
-# program: 2,4,1,5,7,5,1,6,0,3,4,6,5,5,3,0
-#
-# B = A % 8
-# B = B ^ 5
-# C = A >> B
-# B = B ^ 6
-# A = A >> 3
-# B = B ^ C
-# out( B % 8 )
-# jump 0 if A != 0
+real = [2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 6, 5, 5, 3, 0]
 
-def solve(prog, a, b, c):
-    if prog == []:
-        return b
-    ans = float("inf")
-    for b in range(8):
-        b = (a % 8)
+
+def real_prog(a, b, c) -> list[int]:
+    out = []
+    while a != 0:
+        b = a % 8
         b = b ^ 5
-        c = (a >> b)
+        c = a // (2**b)
         b = b ^ 6
-        a = (a >> 3)
+        a = a // (2**3)
         b = b ^ c
-        if b == prog[-1]:
-            ans = min(ans, solve(prog[:-1], (a << 3) | b, b, c))
-    return ans
+        out.append(b % 8)
+    return out
 
-program = [2,4,1,5,7,5,1,6,0,3,4,6,5,5,3,0]
-for a in range(8):
-    b = 0
-    c = 0
-    print(solve(program, a, b, c))
+
+B = 0
+C = 0
+A = 0
+prog = list(real)
+good = {0}
+for i in range(1, len(prog)):
+    goal = prog[-i:]
+    new_good = set()
+    for ii in range(8):
+        for ok in good:
+            out: list[int]
+            out = real_prog(ok * 8 + ii, B, C)
+            if out == goal:
+                new_good.add(ok * 8 + ii)
+    good = new_good.copy()
+    print(good)
+    A = min(good)
+
+final = set()
+for i in range(8):
+    for num in good:
+        goal = [2, 4, 1, 5, 7, 5, 1, 6, 0, 3, 4, 6, 5, 5, 3, 0]
+        out = real_prog(num * 8 + i, 0, 0)
+        if out == goal:
+            final.add(num * 8 + i)
+print(min(final))
