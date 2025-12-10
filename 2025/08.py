fc779ab2b3ff28922605cb150e522c641c687189
Oscar Melin <48945939+oscarm3l1n@users.noreply.github.com>
2025-12-10 09:15:25 +0100

    Add files via upload

--- 2025/08.py
+++ 2025/08.py
@@ -1,44 +1,79 @@
 import sys
+import math
 from collections import defaultdict
 
-D = sys.stdin.read().rstrip()
+D = sys.stdin.read()
 vs = []
 for ln in D.splitlines():
-    x, y, z = list(map(int,ln.split(",")))
-    vs.append((x,y,z))
+    x, y, z = list(map(int, ln.split(",")))
+    vs.append((x, y, z))
 
-X = []
-for i in range(len(vs)):
-    for j in range(i+1, len(vs)):
-        x1,y1,z1 = vs[i]
-        x2,y2,z2 = vs[j]
-        d = (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
-        X.append((d, i, j))
+N = len(vs)
 
-X = sorted(X)
 
-union_find = {i: i for i in range(len(vs))}
+def calc_dist(a, b):
+    x1, y1, z1 = a
+    x2, y2, z2 = b
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
 
-def find(x):
-    if x == union_find[x]:
-        return x
-    union_find[x] = find(union_find[x])
-    return union_find[x]
 
-def mix(x, y):
-    union_find[find(x)] = find(y)
+V = []
+for i in range(N):
+    for j in range(N):
+        if i < j:
+            d = calc_dist(vs[i], vs[j])
+            V.append((d, i, j))
 
-connections = 0
-for i, (_, x, y) in enumerate(X):
-    if i == 1000:
-        count = defaultdict(int)
-        for x in range(len(vs)):
-            count[find(x)] += 1
-        S = sorted(count.values())
-        print(S[-1] * S[-2] * S[-3])
-    if find(x) != find(y):
-        connections += 1
-        if connections == len(vs)-1:
-            print(vs[x][0] * vs[y][0])
-        mix(x, y)
+V = sorted(V)
 
+UnionFind = {i: i for i in range(N)}
+
+
+def find(node):
+    p = UnionFind[node]
+    while p != UnionFind[p]:
+        UnionFind[p] = UnionFind[UnionFind[p]]
+        p = UnionFind[p]
+    return p
+
+
+def union(x, y):
+    root_x = find(x)
+    root_y = find(y)
+    UnionFind[root_x] = root_y
+
+
+def p1():
+    for _, i, j in V[:1000]:
+        union(i, j)
+
+    C = defaultdict(int)
+    for i in range(N):
+        C[find(i)] += 1
+
+    S = sorted(C.values())
+    print(S[-3] * S[-2] * S[-1])
+
+
+def p2():
+    global UnionFind
+    conns = 0
+    V = []
+    for i in range(N):
+        for j in range(N):
+            if i < j:
+                d = calc_dist(vs[i], vs[j])
+                V.append((d, i, j))
+
+    V = sorted(V)
+    UnionFind = {i: i for i in range(len(V))}
+    for _, i, j in V:
+        if find(i) != find(j):
+            conns += 1
+            if conns == len(vs) - 1:
+                print(vs[i][0] * vs[j][0])
+            union(i, j)
+
+
+p1()
+p2()
